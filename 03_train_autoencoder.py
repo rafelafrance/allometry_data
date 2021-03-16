01bffb178b2a81f0b762d335f78bb6d799bb2d0d
rafe <raphael.lafrance@gmail.com>
2021-03-16 10:09:01 -0400

    Allow restarting of model training

--- 03_train_autoencoder.py
+++ 03_train_autoencoder.py
@@ -11,6 +11,7 @@ import torch
 import torch.optim as optim
 from torch import nn
 from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 
 from allometry.autoencoder import Autoencoder
@@ -21,8 +22,10 @@ from allometry.util import finished, started
 def train(args):
     """Train the neural net."""
     logging.info('Starting training')
+    writer = SummaryWriter()
 
     model = Autoencoder()
+    load_model(args, model)
 
     device = torch.device(args.device)
     model.to(device)
@@ -39,14 +42,17 @@ def train(args):
 
     for epoch in range(1, args.epochs + 1):
         train_batches(model, device, criterion, losses, train_loader, optimizer)
-        msg = train_log(losses)
+        msg = train_log(writer, losses, epoch)
         losses = []
 
         valid_batches(model, device, criterion, losses, valid_loader)
-        curr_valid = valid_log(epoch, losses, msg)
+        curr_valid = valid_log(writer, losses, epoch, msg)
         losses = []
 
         save_model(args, model, epoch, best_valid, curr_valid)
+
+    writer.flush()
+    writer.close()
 
 
 def train_batches(model, device, criterion, losses, loader, optimizer):
@@ -76,23 +82,35 @@ def valid_batches(model, device, criterion, losses, loader):
             losses.append(batch_loss.item())
 
 
-def train_log(losses):
+def train_log(writer, losses, epoch):
     """Clean up after the training epoch."""
-    return f'training {np.mean(losses):0.6f}'
+    avg_loss = np.mean(losses)
+    writer.add_scalar("Loss/train", avg_loss, epoch)
+    return f'training {avg_loss:0.6f}'
 
 
-def valid_log(epoch, losses, msg):
+def valid_log(writer, losses, epoch, msg):
     """Clean up after the validation epoch."""
     avg_loss = np.mean(losses)
+    writer.add_scalar("Loss/valid", avg_loss, epoch)
     logging.info(f'Epoch: {epoch: 3d} Average losses {msg} validation {avg_loss:0.6f}')
     return avg_loss
 
 
 def save_model(args, model, epoch, best_valid, curr_valid):
     """Save the model if the current validation score is better than the best one."""
-    if curr_valid < best_valid and epoch % args.check_point == 0:
-        path = args.model_dir / f'best_{epoch}.pth'
+    # TODO save optimizer too
+    if curr_valid < best_valid and epoch % args.save_every == 0:
+        path = args.model_dir / f'best_autoencoder{epoch}.pth'
+        model.state_dict()['epoch'] = epoch
         torch.save(model.state_dict(), path)
+
+
+def load_model(args, model):
+    """Load a saved model."""
+    if args.load_model:
+        state = torch.load(args.load_model)
+        model.load_state_dict(state)
 
 
 def get_loaders(args):
@@ -170,12 +188,17 @@ def parse_args():
         help="""Input batch size. (default: %(default)s)""")
 
     arg_parser.add_argument(
-        '--check-point', '-c', type=int, default=10,
-        help="""Check point every c iterations. (default: %(default)s)""")
+        '--save-every', '-i', type=int, default=10,
+        help="""Check every -i iterations to see if we should save a snapshot of the
+            model. It only saves if the validation loss is less than the previous
+            best validation loss. (default: %(default)s)""")
 
     arg_parser.add_argument(
         '--workers', '-w', type=int, default=4,
         help="""Number of workers for loading data. (default: %(default)s)""")
+
+    arg_parser.add_argument(
+        '--load-model', '-L', help="""Load this state dict to restart the model.""")
 
     args = arg_parser.parse_args()
 
