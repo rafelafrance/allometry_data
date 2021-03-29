83714835e783e2142de099998e9fd9a51e03ff85
rafe <raphael.lafrance@gmail.com>
2021-03-29 13:27:07 -0400

    Update generate images bash script

--- 02_generate_images.bash
+++ 02_generate_images.bash
@@ -4,3 +4,5 @@
 ./02_generate_images.py @args/02_generate_train_images.args --seed=9976 --text-dir ./data/page/pool2 &
 ./02_generate_images.py @args/02_generate_train_images.args --seed=3048 --text-dir ./data/page/pool3 &
 ./02_generate_images.py @args/02_generate_train_images.args --seed=7712 --text-dir ./data/page/pool4 &
+./02_generate_images.py @args/02_generate_valid_images.args --text-dir ./data/page/pool_valid &
+./02_generate_images.py @args/02_generate_test_images.args --text-dir ./data/page/pool_test &
