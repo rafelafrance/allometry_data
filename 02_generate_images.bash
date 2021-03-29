75a80c830d9f7bd18462805752e010cecb3a3846
rafe <raphael.lafrance@gmail.com>
2021-03-29 14:14:33 -0400

    Update image generation arguments

--- 02_generate_images.bash
+++ 02_generate_images.bash
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-./02_generate_images.py @args/02_generate_train_images.args --seed=4512 --text-dir ./data/page/pool1 &
-./02_generate_images.py @args/02_generate_train_images.args --seed=9976 --text-dir ./data/page/pool2 &
-./02_generate_images.py @args/02_generate_train_images.args --seed=3048 --text-dir ./data/page/pool3 &
-./02_generate_images.py @args/02_generate_train_images.args --seed=7712 --text-dir ./data/page/pool4 &
-./02_generate_images.py @args/02_generate_valid_images.args --text-dir ./data/page/pool_valid &
-./02_generate_images.py @args/02_generate_test_images.args --text-dir ./data/page/pool_test &
+./02_generate_images.py @args/02_generate_train_images.args --seed=4512 --text-dir=./data/page/pool_train_1 &
+./02_generate_images.py @args/02_generate_train_images.args --seed=9976 --text-dir=./data/page/pool_train_2 &
+./02_generate_images.py @args/02_generate_train_images.args --seed=2048 --text-dir=./data/page/pool_train_3 &
+./02_generate_images.py @args/02_generate_train_images.args --seed=7712 --text-dir=./data/page/pool_train_4 &
+./02_generate_images.py @args/02_generate_valid_images.args &
+./02_generate_images.py @args/02_generate_test_images.args &
