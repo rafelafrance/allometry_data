743947a31e70830230a30ad38b33d9a5073bf21a
rafe <raphael.lafrance@gmail.com>
2021-04-06 09:57:28 -0400

    Sort chars so that we can move them to different categories

--- allometry/const.py
+++ allometry/const.py
@@ -20,7 +20,7 @@ CHAR_IMAGE_SIZE = ImageSize(32, 48)
 
 TINY_PUNCT = '.-,'
 OTHER_PUNCT = """$%*()<=>+/:"""
-CHARS = list(string.digits + string.ascii_uppercase + TINY_PUNCT + OTHER_PUNCT)
+CHARS = sorted(list(string.digits + string.ascii_uppercase + TINY_PUNCT + OTHER_PUNCT))
 CHAR_TO_CLASS = {c: i for i, c in enumerate(CHARS)}
 CLASS_TO_CHAR = {v: k for k, v in CHAR_TO_CLASS.items()}
 
